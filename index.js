a33d2d2f92cc1cb83dd7363ae2a6e52bc3d43fc3
Peter V <peter@vavro.me>
2019-03-05 17:30:11 -0500

    Bug fixed : It was not closing some of the open sessions.

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 'use strict';
 
-const pkg = require("./package.json")
-const OrientDBClient = require('orientjs').OrientDBClient
+const pkg = require("./package.json");
+const OrientDBClient = require('orientjs').OrientDBClient;
 
 exports.plugin = {
     pkg,
     register: async function (server, options) {
+
+        // Default values
+        let initOnRequestLifecycleStep = 'onRequest';
+
+        if (typeof options.initOnRequestLifecycleStep !== 'undefiend' &&
+            options.initOnRequestLifecycleStep !== undefined &&
+            options.initOnRequestLifecycleStep !== '') {
+
+            initOnRequestLifecycleStep = options.initOnRequestLifecycleStep;
+        };
 
         const config = {
             host: options.host,
             port: options.port,
             db: options.db,
             user: options.username,
-            password: options.password
-        }
+            password: options.password,
+        };
 
         /**
          * Init connection
@@ -26,7 +36,7 @@ exports.plugin = {
                 pool: {
                     max: 10
                 }
-            })
+            });
 
             let pool = await client.sessions({
                 name: config.db,
@@ -35,66 +45,79 @@ exports.plugin = {
                 pool: {
                     max: 25
                 }
-            })
+            });
 
-            return { client, pool }
+            return { client, pool };
 
-        }
+        };
 
         const boostrap = ({ client, pool }) => {
 
             server.ext({
-                type: 'onRequest',
+                type: initOnRequestLifecycleStep,
                 method: async function (request, h) {
 
-                    // Get session from pool
-                    let session = await pool.acquire()
+                    // Get session from pool and pass to request
+                    request.app.OrientDB = await pool.acquire();
 
                     // Logging
-                    server.log([pkg.name], 'Session acquired.');
-
-                    // Pass to request
-                    request.app.OrientDB = session
+                    server.log([pkg.name, initOnRequestLifecycleStep], `Session acquired.`);
 
                     // On finish
-                    request.events.once('finish', async function (a) {
-
-                        // Release the session
-                        await session.close()
-
-                        // Close the pool
-                        // TODO :: await pool.close(); // https://github.com/orientechnologies/orientjs/issues/360
+                    request.events.once('disconnect', async function () {
 
                         // Log
-                        server.log([pkg.name], 'Session closed.');
+                        server.log([pkg.name, 'disconnect'], `Session closed. ${pool.pending()} sessions open.`);
+
+                        // Release the session
+                        await request.app.OrientDB.close();
+
+                        console.error('request aborted');
 
                     })
 
-                    return h.continue
+                    return h.continue;
 
                 }
 
-            })
+            });
+
+            server.ext({
+                type: 'onPreResponse',
+                method: async function (request, h) {
+
+                    // Release the session
+                    await request.app.OrientDB.close();
+
+                    // Close the pool
+                    // TODO :: await pool.close(); // https://github.com/orientechnologies/orientjs/issues/360
+
+                    // Log
+                    server.log([pkg.name, 'onPreResponse'], `Session closed. ${pool.pending()} sessions open.`);
+
+                    return h.continue;
+                }
+            });
 
             process.on('SIGINT', async function (a, b) {
 
                 // Close client
-                await client.close()
+                await client.close();
 
-            })
+            });
 
         }
 
         // Init process method
         const run = async () => {
 
-            let { client, pool } = await setupDatabase()
+            let { client, pool } = await setupDatabase();
 
-            boostrap({ client, pool })
+            boostrap({ client, pool });
 
-        }
+        };
 
         // Run
-        run()
+        run();
     }
 };
